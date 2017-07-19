dc6683cd6ee0c2f66e3da8b37f0d86aab99375a4
farrradise <marzini.f@gmail.com>
2017-07-19 15:35:29 +0200

    tried to do the math

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-// variables
-nbrTemp="";
-leChiffre = document.getElementsByClassName("chiffre");
-operateur = document.getElementsByClassName('operateur');
-displayResult = document.getElementsByTagName("th")[0];
-nbr1 ="";
+var nbrTemp="";
+var leChiffre = document.getElementsByClassName("chiffre");
+var operateur = document.getElementsByClassName('operateur');
+var displayResult = document.getElementsByTagName("th")[0];
+var nbr1 ="";
+var nbr2 ="";
+var ope = "";
 
 // pour parcourir le tableau au clic d'un chiffre
 for (let i = 0; i < leChiffre.length; i++) {
@@ -13,7 +14,7 @@ for (let i = 0; i < leChiffre.length; i++) {
 }
 
 //pour recommencer dès le début
-document.getElementById('reboot').addEventListener("click", function () {displayResult.innerHTML = 0; nbrTemp="";});
+document.getElementById('reboot').addEventListener("click", function () {displayResult.innerHTML = 0; nbrTemp=""; nbr1 =""; nbr2="";});
 
 
 // fonction pour cliquer et afficher les chiffres
@@ -30,12 +31,33 @@ function afficheChiffre (th, index) {
 
 //fonction pour cliquer et afficher les opérateurs
 function allowOperateur(th, index) {
+  ope = th.textContent;
+  switch (ope) {
+    case "+": ope ="+"; break;
+    case "-": ope ="-"; break;
+    case "×": ope ="*"; break;
+    case "÷": ope ="/"; break;
+
+    default:
+
+  }
+  // if (ope == "=") {
+    // calcul(nbr1, ope, nbr2);
+  // }
   if (nbr1=="") {
     nbr1 = nbrTemp;
     nbrTemp ="";
   } else {
-    alert(nbr1 + " + " + nbrTemp);
-    // nbrTemp="";
-    // sinon déjà une valeur dans var nbr1 faire appel à la fonction du calcul
+    nbr2 = nbrTemp;
+    nbrTemp = "";
+    // alert(ope);
+    calcul(nbr1, ope, nbr2);
   }
+}
+
+
+// fonction pour faire le calcul
+function calcul(number1, operator, number2) {
+  nbr1 = eval(number1 + operator + number2);
+  displayResult.innerHTML = nbr1;
 }
