f5f7b74178d29e4f82c6a16924d6976ea417feb7
farrradise <marzini.f@gmail.com>
2017-07-19 19:04:33 +0200

    equal function okay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var nbrTemp="";
 var leChiffre = document.getElementsByClassName("chiffre");
 var operateur = document.getElementsByClassName('operateur');
 var displayResult = document.getElementsByTagName("th")[0];
+var nbrTemp="";
 var nbr1 ="";
 var nbr2 ="";
 var ope = "";
@@ -24,40 +24,54 @@ function afficheChiffre (th, index) {
 
     for (let j = 0; j < operateur.length; j++) {
         operateur[j].addEventListener("click", function() {
-            allowOperateur(this, j);
+            allowOperateur(this);
         });
     }
 }
 
 //fonction pour cliquer et afficher les opérateurs
-function allowOperateur(th, index) {
+function allowOperateur(th) {
   ope = th.textContent;
+
   switch (ope) {
     case "+": ope ="+"; break;
     case "-": ope ="-"; break;
     case "×": ope ="*"; break;
     case "÷": ope ="/"; break;
-
+    // case "=": ope ="="; break;
     default:
-
   }
-  // if (ope == "=") {
-    // calcul(nbr1, ope, nbr2);
-  // }
+    operation();
+}
+
+
+//fonction pour remplir nbr1 et nbr2
+function operation() {
   if (nbr1=="") {
     nbr1 = nbrTemp;
     nbrTemp ="";
   } else {
     nbr2 = nbrTemp;
     nbrTemp = "";
-    // alert(ope);
     calcul(nbr1, ope, nbr2);
   }
 }
-
 
 // fonction pour faire le calcul
 function calcul(number1, operator, number2) {
   nbr1 = eval(number1 + operator + number2);
   displayResult.innerHTML = nbr1;
+  // ope="";
+}
+
+
+// fonction pour le =
+document.getElementById("egal").addEventListener("click", function () { quandAppuiSurEgal();});
+
+function quandAppuiSurEgal() {
+  calcul(nbr1, ope, nbrTemp);
+  // alert(nbr1);
+  // alert(nbrTemp);
+  // alert(ope);
+  // alert("test");
 }
