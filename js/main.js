27dcde9ba974a93ce7c605af98ca5aecd5a8a2cf
farrradise <marzini.f@gmail.com>
2017-07-19 10:30:09 +0200

    first step for calc : display the numbers on click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-document.getElementsByClassName("chiffre").addEventListener
+// variables
+nbrTemp="";
+leChiffre = document.getElementsByClassName("chiffre");
+
+// pour parcourir le tableau au clic d'un chiffre
+for (let i = 0; i < leChiffre.length; i++) {
+    leChiffre[i].addEventListener("click", function() {
+        afficheChiffre(this, i);
+    });
+}
+
+function afficheChiffre (th, index) {
+  nbrTemp += leChiffre[index].textContent;
+  document.getElementsByTagName("th")[0].innerHTML = nbrTemp;
+}
